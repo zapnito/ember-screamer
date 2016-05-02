da500225b20710f5c5af7c5e6273447bec690fa2
opsb <oliver@opsb.co.uk>
2016-05-02 21:48:21 +0200

    Switch to uuid for conversation lookup (needs to be paramterized)

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel() {
-    this.transitionTo('conversation', 'lobby');
+    this.transitionTo('conversation', 'eecf4c2b-f6e5-3ae3-bef7-1ea09f91d3e7');
   }
 });
